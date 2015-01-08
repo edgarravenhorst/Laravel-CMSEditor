5a741fdf3ad7f331d3f62a52e7885d1bca28553a
mathijsjansen <mathijs@webineigenhanden.nl>
2015-01-08 19:00:38 +0100

    angular template

--- app/views/cms/index.blade.php
+++ app/views/cms/index.blade.php
@@ -5,12 +5,13 @@
 	<title>CMS editor</title>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script>
     <script>
-    var cmsEdit = angular.module('cmsEdit', []);
+    var cmsEdit = angular.module('cmsEdit', [])
 
-      cmsEdit.config(function($interpolateProvider) {
-        $interpolateProvider.startSymbol('//');
-        $interpolateProvider.endSymbol('//');
-      });
+    cmsEdit.config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[');
+        $interpolateProvider.endSymbol(']]');
+    });
+
     </script>
 </head>
 <body>
@@ -18,7 +19,7 @@
       <label>Name:</label>
       <input type="text" ng-model="yourName" placeholder="Enter a name here">
       <hr>
-      <h1>Hello //yourName//!</h1>
+      <h1>[[Hello yourName]]!</h1>
     </div>
 </body>
 </html>
