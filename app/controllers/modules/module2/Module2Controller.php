40f9c1dd238606d831e15fc10c59407596b958e0
mathijsjansen <mathijs@webineigenhanden.nl>
2015-01-08 18:39:01 +0100

    setup controllers and models folders

---
+++ app/controllers/modules/module2/Module2Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class ModuleTwocontroller extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| ModuleTwocontroller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|
+	*/
+
+}
