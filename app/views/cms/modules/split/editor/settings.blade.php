2fcaf657c80ccb67d07fa208a9f295d14c9c2f2c
Edgar Ravenhorst <edgar@edge-art.nl>
2015-01-08 22:42:55 +0100

    implement js + sass into modules

---
+++ app/views/cms/modules/split/editor/settings.blade.php
@@ -0,0 +1,12 @@
+<section id='module-split' class='module'>
+
+    <h2>Split</h2>
+    <button><back</button>
+
+    <button class='orientation'>Split Horizontal</button>
+    <br />
+    <button class='orientation'>Split Vertical</button>
+
+    <script src="{{ URL::asset('assets/js/cms/modules/split/module.js') }}"></script>
+
+</section>
---
+++ public/assets/js/cms/modules/split/module.js
@@ -0,0 +1,6 @@
+alert('huh');
+
+$('#module-split button').click(function(e){
+    alert('split-it');
+    $('#cms-editor-interface').append('<span class="splitLine"></span>');
+});
