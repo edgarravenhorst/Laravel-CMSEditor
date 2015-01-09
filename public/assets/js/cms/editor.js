e2ef0c9957ee0c3bec007b821a538e17da9b4f29
Edgar Ravenhorst <edgar@edge-art.nl>
2015-01-09 17:12:02 +0100

    Handle test, nog niet bug vrij

--- public/assets/js/cms/editor.js
+++ public/assets/js/cms/editor.js
@@ -25,8 +25,70 @@ function InterfaceCtrl($scope, $compile, moduleService){
     $scope.createAddModuleButton = function(container){
         container.parent('.wrapper').find('.btn-add-module').remove();
         var btnhtml = '<button class="btn-add-module" ng-click="addModule($event, \'#module-select\')"></button>'
-        var temp = $compile(btnhtml)($scope);
-        container.append(temp);
+        container.append($compile(btnhtml)($scope));
+
+
+        $('.wrapper').find('.handle').remove();
+        $.each($('.wrapper'), function(i,wrapper){
+            var position = {
+                top: parseInt($(wrapper).css('top'),10),
+                left: parseInt($(wrapper).css('left'),10),
+                right: parseInt($(wrapper).css('right'),10),
+                bottom: parseInt($(wrapper).css('bottom'),10),
+            }
+
+            if (position.right > 0) {
+                var handlehtml = '<button class="handle handle-right"></button>'
+                $(wrapper).append(handlehtml);
+            }
+
+            if (position.bottom > 0) {
+                var handlehtml = '<button class="handle handle-bottom"></button>'
+                $(wrapper).append(handlehtml);
+            }
+        });
+
+        $('.handle').unbind('mousedown');
+        $('.handle').unbind('mouseup');
+        $('.handle').mousedown(function(e){
+
+            var wrapper1 = $(this).closest('.wrapper');
+            var wrapcontainer = wrapper1.parent()
+            var wrapper2 = wrapcontainer
+
+            $.each(wrapcontainer.children(), function(i,wrapper){
+                if (wrapper != wrapper1)
+                    wrapper2 = $(wrapper);
+            })
+
+            var position = {
+                right: parseInt($(wrapper1).css('right'),10),
+                bottom: parseInt($(wrapper1).css('bottom'),10),
+            }
+
+            wrapcontainer.mousemove(function(e){
+                offsetX = e.clientX - wrapcontainer.position().left;
+                offsetY = e.clientY - wrapcontainer.position().top;
+
+                if (position.right > 0){
+                    wrapper1.css('right', 100-(offsetX/wrapcontainer.outerWidth())*100 + '%');
+                    wrapper2.css('left', (offsetX/wrapcontainer.outerWidth())*100 + '%');
+                }
+
+                if (position.bottom > 0){
+                    wrapper1.css('bottom', 100-(offsetY/wrapcontainer.outerHeight())*100 + '%');
+                    wrapper2.css('top', (offsetY/wrapcontainer.outerHeight())*100 + '%');
+                }
+
+
+            })
+
+            $(document).mouseup(function(e){
+                wrapcontainer.unbind('mousemove');
+            });
+        })
+
+
     }
 
     $scope.compileChildWrappers = function(){
